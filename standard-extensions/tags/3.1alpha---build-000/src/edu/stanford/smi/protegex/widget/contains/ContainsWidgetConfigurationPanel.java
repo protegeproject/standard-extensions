556f8978a5ecaf9df31e11ac1e53b4ac8e814493
vendetti <vendetti@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2005-03-02 00:03:03 +0000

    tagged by automatic build

---
+++ standard-extensions/tags/3.1alpha---build-000/src/edu/stanford/smi/protegex/widget/contains/ContainsWidgetConfigurationPanel.java
@@ -0,0 +1,20 @@
+package edu.stanford.smi.protegex.widget.contains;
+
+import edu.stanford.smi.protege.widget.*;
+import edu.stanford.smi.protegex.util.*;
+
+/**
+ *  Description of the class
+ *
+ * @author    William Grosso <grosso@smi.stanford.edu>
+ */
+public class ContainsWidgetConfigurationPanel extends WidgetConfigurationPanel {
+
+    public ContainsWidgetConfigurationPanel(ContainsWidget widget) {
+        super(widget);
+        addTab("Action Buttons", new SixButtonsConfigurationPanel(widget.getState()));
+        addTab("Appearance", new OverallAppearanceConfigurationPanel(widget.getState()));
+        addTab("Instance Creation", new PrototypingAndCreationConfigurationPanel(widget.getState()));
+        addTab("Summary Panel", new SummaryConfigurationPanel(widget.getState()));
+    }
+}
