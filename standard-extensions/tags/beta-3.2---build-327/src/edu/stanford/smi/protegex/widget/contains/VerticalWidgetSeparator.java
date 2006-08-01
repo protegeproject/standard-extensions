493d13eceefca87d3feead8b24f0a7c4cf8ef9d0
vendetti <vendetti@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-08-01 01:17:25 +0000

    tagged by automatic build

---
+++ standard-extensions/tags/beta-3.2---build-327/src/edu/stanford/smi/protegex/widget/contains/VerticalWidgetSeparator.java
@@ -0,0 +1,25 @@
+package edu.stanford.smi.protegex.widget.contains;
+
+import java.awt.*;
+
+/**
+ *  Description of the Class
+ *
+ * @author    William Grosso <grosso@smi.stanford.edu>
+ */
+public class VerticalWidgetSeparator extends WidgetSeparator {
+
+    public VerticalWidgetSeparator(int divisor) {
+        super(divisor);
+    }
+
+    public void paint(Graphics g) {
+        Rectangle paintingArea = getBounds();
+        paintingArea.height = (paintingArea.height) / _divisor;
+        paintingArea.x = 5;
+        paintingArea.width -= 10;
+        paintingArea.y = paintingArea.height;
+        g.setColor(Color.black);
+        g.fillRoundRect(paintingArea.x, paintingArea.y, paintingArea.width, paintingArea.height, 3, 3);
+    }
+}
