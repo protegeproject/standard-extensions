8395476f7839f3a556069b3f5c4c95ba6c148581
vendetti <vendetti@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-10-11 22:56:47 +0000

    tagged by automatic build

---
+++ standard-extensions/tags/beta-3.2---build-340/src/edu/stanford/smi/protegex/queries_tab/toolbox/InstancesQuery.java
@@ -0,0 +1,16 @@
+package edu.stanford.smi.protegex.queries_tab.toolbox;
+
+public class InstancesQuery extends AbstractQuery {
+
+    public InstancesQuery(boolean b) {
+        super(b);
+    }
+
+    public void initialize() {
+        // Abstract member function
+    }
+
+    public String toString() {
+        return getName();
+    }
+}
