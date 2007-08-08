bddda8d3a13f588fd8677a1718ef5e1178aa724e
Tania Tudorache <tudorache@users.noreply.github.com>
2007-08-08 19:32:25 +0000

    made the query export configurable

--- src/edu/stanford/smi/protegex/queries_tab/InstancesList.java
+++ src/edu/stanford/smi/protegex/queries_tab/InstancesList.java
@@ -15,12 +15,15 @@ import javax.swing.JScrollPane;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import com.sun.corba.se.spi.legacy.connection.GetEndPointInfoAgainException;
+
 import edu.stanford.smi.protege.action.ReferencersAction;
 import edu.stanford.smi.protege.model.*;
 import edu.stanford.smi.protege.resource.Icons;
 import edu.stanford.smi.protege.resource.ResourceKey;
 import edu.stanford.smi.protege.ui.DisplayUtilities;
 import edu.stanford.smi.protege.ui.ListFinder;
+import edu.stanford.smi.protege.ui.ProjectManager;
 import edu.stanford.smi.protege.util.*;
 
 /**
@@ -34,10 +37,11 @@ import edu.stanford.smi.protege.util.*;
  */
 
 public class InstancesList extends SelectableContainer implements Disposable {
-	
 	private Project itsProject;
     private LabeledComponent c;
     private SelectableList itsList;
+    private ExportConfigurationPanel exportConfigurationPanel;
+    
 
     public InstancesList(Project project) {
         itsProject = project;
@@ -120,58 +124,31 @@ public class InstancesList extends SelectableContainer implements Disposable {
 		return new AbstractAction("Export Slot Values to file", Icons.getQueryExportIcon()) {
 
 			public void actionPerformed(ActionEvent e) {
-				// Strip out system slots
-				HashSet<Slot> slots = new HashSet<Slot>();
-				Iterator j = itsProject.getKnowledgeBase().getSlots()
-						.iterator();
-				while (j.hasNext()) {
-					Slot s = (Slot) j.next();
-					if (!s.isSystem()) {
-						slots.add(s);
-					}
-				}
-
-				// Show util window for multiple slot selection
-				Collection slotsToExport = DisplayUtilities.pickSlots(InstancesList.this, slots,
-						"Select slots to export (multiple selection)");
-
-				File fileToSave = null;
-
-				JFileChooser chooser = ComponentFactory.createFileChooser(
-						"Select file to export query results ",
-						"Exported result files", "csv");
-				int saveDialogResult = chooser.showSaveDialog(InstancesList.this);
-				switch (saveDialogResult) {
-				case JFileChooser.ERROR_OPTION:
-					break;
-				case JFileChooser.CANCEL_OPTION:
-					break;
-				case JFileChooser.APPROVE_OPTION:
-					fileToSave = chooser.getSelectedFile();
-					break;
-				default:
-					Assert.fail("bad result: " + saveDialogResult);
-					break;
-				}
-
-				if (fileToSave == null) {
+				exportConfigurationPanel = getExportConfigurationPanel();
+				
+				int sel = ModalDialog.showDialog(ProjectManager.getProjectManager().getCurrentProjectView(), exportConfigurationPanel.getConfigPanel(), "Export configuration", ModalDialog.MODE_OK_CANCEL);
+				
+				if (sel == ModalDialog.OPTION_CANCEL) {
 					return;
 				}
-
+				
+				File fileToSave = exportConfigurationPanel.getExportedFile();
+				Collection<Slot> slotsToExport = exportConfigurationPanel.getExportedSlots(); 
+					
 				boolean success = false;
 				
 				try {
 					Writer outputStream = FileUtilities.createBufferedWriter(fileToSave);
 
 					if (outputStream == null) {
-						Log.getLogger().log(Level.WARNING, "Unable to open output file.");
+						Log.getLogger().log(Level.WARNING, "Unable to open output file " + fileToSave + ".");
 					} else {
 						printResults(outputStream, getModel().getValues(), slotsToExport);
 						success = true;
 					}
 
 				} catch (Exception ex) {
-					Log.getLogger().log(Level.WARNING, "Errors at writing out query results file.", ex);					
+					Log.getLogger().log(Level.WARNING, "Errors at writing out query results file " + fileToSave + ".", ex);					
 				}
 				
 				String messageText = success ? "Query results exported successfully to:\n" + fileToSave.getAbsolutePath() :
@@ -183,12 +160,27 @@ public class InstancesList extends SelectableContainer implements Disposable {
 			}
 		};
 	}
-		
+	
+    
+    private ExportConfigurationPanel getExportConfigurationPanel() {
+    	if (exportConfigurationPanel != null) {
+    		return exportConfigurationPanel;
+    	}
+    	
+    	exportConfigurationPanel = new ExportConfigurationPanel(itsProject.getKnowledgeBase());
+    	return exportConfigurationPanel;
+    }
 	
 
 	private void printResults(Writer writer, Collection instances, 
 									 Collection slots) {
 	    PrintWriter output = new PrintWriter(writer);
+	    
+	    if (getExportConfigurationPanel().isExportHeaderEnabled()) {
+	    	//prints the slot names as the first line in the exported file
+	    	printHeader(output, slots);
+	    }
+	    
 	    Iterator i = instances.iterator();
 	    while (i.hasNext()) {
 	        Instance instance = (Instance) i.next();
@@ -197,23 +189,45 @@ public class InstancesList extends SelectableContainer implements Disposable {
 	    output.close();
 	}
 	
-	private static void addInstance(PrintWriter writer, Instance instance, 
+	private void printHeader(PrintWriter writer, Collection slots) {
+		StringBuffer buffer = new StringBuffer();
+		
+        buffer.append("Instance");
+        buffer.append(getExportConfigurationPanel().getSlotDelimiter());
+        
+        if (getExportConfigurationPanel().isExportTypesEnabled()) {
+            buffer.append("Type(s)");
+            buffer.append(getExportConfigurationPanel().getSlotDelimiter());        	
+        }
+        
+        for (Iterator iter = slots.iterator(); iter.hasNext();) {
+			Slot slot = (Slot) iter.next();
+			buffer.append(slot.getBrowserText());
+			buffer.append(getExportConfigurationPanel().getSlotDelimiter());
+		}
+        
+        writer.println(buffer.toString());
+	}
+	
+	private void addInstance(PrintWriter writer, Instance instance, 
 									Collection slots) {
         StringBuffer buffer = new StringBuffer();
 
         // Export the browser text for the current instance.
         buffer.append(instance.getBrowserText());
-        buffer.append("\t");
-        
-        // Export the direct types for the current instance.
-        Collection directTypes = instance.getDirectTypes();
-        Iterator i = directTypes.iterator();
-        while (i.hasNext()) {
-        	Cls directType = (Cls) i.next();
-        	buffer.append(directType.getBrowserText());
-        	if (i.hasNext()) {
-        		buffer.append("\t");
-        	}
+                
+        if (getExportConfigurationPanel().isExportTypesEnabled()) {
+        	buffer.append(getExportConfigurationPanel().getSlotDelimiter());
+	        // Export the direct types for the current instance.
+	        Collection directTypes = instance.getDirectTypes();
+	        Iterator i = directTypes.iterator();
+	        while (i.hasNext()) {
+	        	Cls directType = (Cls) i.next();
+	        	buffer.append(directType.getBrowserText());
+	        	if (i.hasNext()) {
+	        		buffer.append(getExportConfigurationPanel().getSlotValuesDelimiter());
+	        	}
+	        }
         }
         
         // Export the own slot values for each slot attached to the 
@@ -225,10 +239,8 @@ public class InstancesList extends SelectableContainer implements Disposable {
             while (j.hasNext()) {
             	Slot slot = (Slot) j.next();
 
-            	Collection values = instance.getOwnSlotValues(slot);
-            	if (!values.isEmpty()) {
-            		buffer.append("\t");
-            	}
+            	Collection values = instance.getOwnSlotValues(slot);            	
+            	buffer.append(getExportConfigurationPanel().getSlotDelimiter());            	
             	
             	// Loop through values for particular slot.
             	Iterator k = values.iterator();
@@ -241,7 +253,7 @@ public class InstancesList extends SelectableContainer implements Disposable {
             		buffer.append(value);
             		
             		if (k.hasNext()) {
-            			buffer.append("\t");
+            			buffer.append(getExportConfigurationPanel().getSlotValuesDelimiter());
             		}
             	}
             }
