532d63d1a5a2fcc98e84e5fc80e09f2c469f658d
Tania Tudorache <tudorache@users.noreply.github.com>
2008-07-21 22:12:23 +0000

    better error handling. Minor refactor. Java 1.5

--- src/edu/stanford/smi/protegex/export/html/HTMLExportPlugin.java
+++ src/edu/stanford/smi/protegex/export/html/HTMLExportPlugin.java
@@ -2,12 +2,18 @@ package edu.stanford.smi.protegex.export.html;
 
 import java.awt.Dimension;
 import java.awt.Window;
+import java.io.File;
+import java.util.logging.Level;
+
 import javax.swing.JComponent;
 import javax.swing.SwingUtilities;
 
 import edu.stanford.smi.protege.model.Project;
 import edu.stanford.smi.protege.plugin.ExportPlugin;
+import edu.stanford.smi.protege.plugin.PluginUtilities;
 import edu.stanford.smi.protege.ui.ProjectManager;
+import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protege.util.ModalDialog;
 
 /**
  *
@@ -21,30 +27,53 @@ public class HTMLExportPlugin implements ExportPlugin {
     }
 
     public void handleExportRequest(Project project) {
-        JComponent mainPanel = ProjectManager.getProjectManager().getMainPanel();
-        Window window = SwingUtilities.windowForComponent(mainPanel);
-        if (window instanceof java.awt.Frame) {
-            dialog = new HTMLExportConfigDialog((java.awt.Frame) window,
-                "HTML Export Configuration Options", true, project);
-            dialog.setSize(new Dimension(450, 525));
-            dialog.setLocationRelativeTo(mainPanel);
-            dialog.setVisible(true);
+    	JComponent mainPanel = ProjectManager.getProjectManager().getMainPanel();
+    	Window window = SwingUtilities.windowForComponent(mainPanel);
+    	if (window instanceof java.awt.Frame) {
 
-            if (dialog.okPressed == true) {
-                ExportConfiguration config = dialog.getExportConfiguration();
-                HTMLExport exporter = new HTMLExport(config);
-                try {
-                    exporter.export();
-                } catch (HTMLExportException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        }
+    		String prefix = PluginUtilities.getPluginsDirectory().getPath() +
+    						File.separator +
+    						"edu.stanford.smi.protegex.standard_extensions" +
+    						File.separator +
+    						"html_export" +
+    						File.separator;
+
+    		String path = prefix + "HTMLExportConfigurations.xml";
+
+    		File file = new File(path);
+    		if (!file.exists()) {
+    			ModalDialog.showMessageDialog(window, "Export configuration file not found at:\n" + path
+    					, "Error");
+    			return;
+    		}
+
+    		dialog = new HTMLExportConfigDialog((java.awt.Frame) window,
+    				"HTML Export Configuration Options", true, project, file);
+    		dialog.setSize(new Dimension(450, 525));
+    		dialog.setLocationRelativeTo(mainPanel);
+    		dialog.setVisible(true);
+
+    		if (dialog.okPressed == true) {
+    			ExportConfiguration config = dialog.getExportConfiguration();
+    			HTMLExport exporter = new HTMLExport(config);
+    			try {
+    				exporter.export();
+    				ModalDialog.showMessageDialog(mainPanel, "HTML export successful. Export path:\n" +
+    						dialog.getExportConfiguration().getOutputDir(), "Export successful");
+    			} catch (HTMLExportException e) {
+    				Log.getLogger().log(Level.WARNING, "There were errors at HTML export.", e);
+    				ModalDialog.showMessageDialog(mainPanel, "There were errors at export.\n" +
+    						"See console for details.", "Errors at export");
+    			}
+    		}
+    	}
     }
-    
+
     public static boolean isSuitable(Project prj) {
-    	if (prj == null) return false;
-    	
+    	if (prj == null) {
+			return false;
+		}
+
         String factoryName = prj.getKnowledgeBase().getClass().getName();
         return factoryName.indexOf(".owl.") == -1;
     }
