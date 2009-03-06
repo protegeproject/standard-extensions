242a6452aa5da04689bdfb411b02e81ba6eda2ca
vendetti <vendetti@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-03-06 02:24:27 +0000

    tagged by automatic build

---
+++ standard-extensions/tags/rc2-3.4---build-529/src/edu/stanford/smi/protegex/widget/instancetable/Action_ViewInstance.java
@@ -0,0 +1,29 @@
+package edu.stanford.smi.protegex.widget.instancetable;
+
+import java.awt.event.*;
+import java.util.*;
+
+import edu.stanford.smi.protege.model.*;
+import edu.stanford.smi.protege.resource.*;
+
+/**
+ *  Description of the Class
+ *
+ * @author    William Grosso <grosso@smi.stanford.edu>
+ */
+public class Action_ViewInstance extends Action_AbstractTableListener {
+
+    public Action_ViewInstance(InstanceTableWidget widget, InstanceTable table) {
+        super((widget.getState()).getViewInstanceButtonTooltip(), Icons.getViewIcon(), widget, table);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        Project project = _widget.getProject();
+        Collection instances = _table.getSelectedInstances();
+        Iterator i = instances.iterator();
+        while (i.hasNext()) {
+            project.show((Instance) i.next());
+        }
+        return;
+    }
+}
